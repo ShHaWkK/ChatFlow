c1e6fb8eddee6e96bd4e26428901ffcdef432bb9
ShHaWkK <51519814+ShHaWkK@users.noreply.github.com>
2024-02-25 17:57:41 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -4,8 +4,10 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e) 
+{
     e.preventDefault();
+    
     if (input.value) {
         socket.emit('chat message', input.value);
         input.value = '';
